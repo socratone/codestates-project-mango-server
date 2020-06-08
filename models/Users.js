9ea310bf46577433aadd0cac5a222eb5f8024ffd
knu9910 <knu991@naver.com>
2020-06-08 15:14:24 +0900

    Mongoodb schema create
     - users 테이블 생성
     - musics 테이블 생성
     - musiclist 테이블 생성

---
+++ models/Users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: mongoose.Schema.Types.ObjectId,
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true },
+  nickname: String,
+}, {
+  versionKey: false
+});
+module.exports = mongoose.model('User', userSchema);
