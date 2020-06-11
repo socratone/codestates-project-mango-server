7601a687903cb12c1ad11ea22baf1a06ab3a8618
knu9910 <knu991@naver.com>
2020-06-11 09:49:06 +0900

    ScoringMusic api rename -> ratingMusic, fix
     - scoringMusic api 이름변경
     - 에러 헨들링 추가 및 findorcreate 적용

--- controllers/ratingMusic.js
+++ controllers/ratingMusic.js
@@ -1,32 +1,34 @@
 const Musics = require('../models').Musics;
 const jwt = require('jsonwebtoken');
-const scoringMusic = async (req, res) => {
+const ratingMusic = async (req, res) => {
   try {
     const {thumbnail, videoid, title, rating} = req.body;
     const access_token = req.headers.authorization.slice(7);
     const secretKey = process.env.TOKEN_KEY;
     const userinfo = await jwt.verify(access_token, secretKey);
     if(userinfo.id) {
-      let music = await Musics.create({
+      const {doc, created} = await Musics.findOrCreate({
         thumbnail,
         videoid,
         title,
         rating,
         user_id: userinfo.id
       });
-      return res.status(201).json(music);
+      return created ? 
+        res.status(201).json(doc) : 
+        res.status(409).end('이미 점수를 매긴 음악입니다');
     }
     return res.status(404).end('잘못된 토큰입니다');
   }
   catch(err){
     if(err.name === 'TokenExpiredError') {
-      res.status(419).json({
+      return res.status(419).json({
         code:419,
         message:'토큰이 만료되었습니다.'
       });
     }
-    console.log(err);
+    return res.status(500).end('Server error');
   }
 };
   
-module.exports = scoringMusic;
+module.exports = ratingMusic;
