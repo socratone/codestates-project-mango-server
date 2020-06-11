9242f0b37911bb08c3543cb25dde8615c576e05b
knu9910 <knu991@naver.com>
2020-06-11 09:54:35 +0900

    AddMusic, deleteMusic implement
     - 리스트에 음악추가 기능 구현 완료
     - 리스트에 음악삭제 기능 구현 완료
     - 에러핸들링 완료

---
+++ controllers/addMusic.js
@@ -0,0 +1,29 @@
+const Musiclists = require('../models').Musiclists;
+const jwt = require('jsonwebtoken');
+const addMusic = async (req, res) => {
+  try{
+    const {thumbnail, videoid, title, listname} = req.body;
+    const access_token = req.headers.authorization.slice(7);
+    const secretKey = process.env.TOKEN_KEY;
+    const userinfo = await jwt.verify(access_token, secretKey);
+    if(userinfo.id) {
+      const {nModified} = await Musiclists.update({listname, user_id: userinfo.id},{$addToSet: {musics: {videoid, title, thumbnail}}});
+      return nModified ?
+        res.status(200).json({thumbnail, videoid, title, listname}) :
+        res.status(409).end('리스트에 이미 존재하는 노래입니다.');
+    }
+    return res.status(404).end('잘못된 토큰입니다');
+  }
+  catch(err) {
+    if(err.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code:419,
+        message:'토큰이 만료되었습니다.'
+      });
+    }
+    console.log(err);
+    return res.status(500).end('Server error');
+  }
+};
+
+module.exports = addMusic;
