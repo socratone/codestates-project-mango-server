896cc4f736d9e7ae4b0713483493ae5703cf1e4b
knu9910 <knu991@naver.com>
2020-06-11 09:48:12 +0900

    Error handling add

--- controllers/refresh.js
+++ controllers/refresh.js
@@ -7,18 +7,19 @@ const refresh = async (req, res) => {
     const refresh_token = req.headers.authorization.slice(7); 
     const secretKey = process.env.TOKEN_KEY;
     await jwt.verify(refresh_token, secretKey);
-    let userinfo = await redis.get(refresh_token);
-    const access_token = await jwt.sign({userinfo}, secretKey, {expiresIn:'30m'});
+    let userinfo = JSON.parse(await redis.get(refresh_token));
+    console.log(userinfo);
+    const access_token = await jwt.sign(userinfo, secretKey, {expiresIn:'30m'});
     res.status(200).json({access_token});
   }
   catch (err) {
     if(err.name === 'TokenExpiredError') {
-      res.status(419).json({
+      return res.status(419).json({
         code:419,
         message:'토큰이 만료되었습니다.'
       });
     }
-    console.log(err);
+    return res.status(500).end('Server error');
   }
 };
 
--- controllers/signin.js
+++ controllers/signin.js
@@ -13,25 +13,23 @@ const signin = async (req, res) => {
     }
     const judge = await bcrypt.compare(password, user.password);
     if(judge) {
-      const userInfo = { email : user.email };
+      const userInfo = { id: user._id, email : user.email };
       const secretKey = process.env.TOKEN_KEY;
-      const options = { expiresIn: '1m'};
+      const options = { expiresIn: '30m'};
       const access_token = await jwt.sign(userInfo, secretKey, options);
       const refresh_token = await jwt.sign({ refresh: true }, secretKey, {expiresIn:'14d'});
-      await redis.set(refresh_token, email);
+      await redis.set(refresh_token, JSON.stringify({email, id:user._id}));
       const userinfo = {email: user.email, nickname: user.nickname};
-      res.status(200).json({
+      return res.status(200).json({
         userinfo,
         access_token,
         refresh_token
       });
-    }else {
-      res.status(404).end('unvalid user');
     }
-    
+    return res.status(404).end('unvalid user');
   }
   catch(err) {
-    res.status(500).end(err);
+    res.status(500).end('Server error');
   }
 };
 
