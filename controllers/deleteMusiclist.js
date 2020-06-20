e22c1a200c9d1fce8c25b5d2fc1b408d41fcf2b7
knu9910 <knu991@naver.com>
2020-06-20 15:11:49 +0900

    Bad Request error add

--- controllers/deleteMusiclist.js
+++ controllers/deleteMusiclist.js
@@ -2,6 +2,9 @@ const Musiclists = require('../models').Musiclists;
 const errorHandling = require('../errorHandling');
 const deleteMusicList = async (req, res) => {
   const { listname } = req.body;
+  if(!listname) {
+    return res.status(400).end('Bad Request');
+  }
   const user_id = await errorHandling(req, res);
   if(!user_id) return;
   let {deletedCount} =await Musiclists.remove({listname, user_id});
