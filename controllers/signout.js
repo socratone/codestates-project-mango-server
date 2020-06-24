17dae80f45034ffbac5503859626074c61d98011
knu9910 <knu991@naver.com>
2020-06-24 20:55:20 +0900

    Add signout Bad Request error

--- controllers/signout.js
+++ controllers/signout.js
@@ -2,6 +2,7 @@ const asyncRedis = require("async-redis");
 const redis = asyncRedis.createClient();
 const signout = async (req, res) => {
   const{access_token, refresh_token} = req.body;
+  if(!access_token || !refresh_token) return res.status(400).end('Bad Request');
   await redis.set(`blacklist_${access_token}`, true);
   await redis.expire(`blacklist_${access_token}`, 60*30);
   await redis.set(`blacklist_${refresh_token}`, true);
